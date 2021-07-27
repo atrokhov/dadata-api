4c53db7e0ec03f2728152a1c5e5d1152a7809018
atrokhov <aatrokhov@gmail.com>
2021-07-28 04:03:28 +0600

    perform method in job was separated by creating private method dadata_request which create request to dadata api, fixed metrics/abcsize for new method, edit credentials for dadata token and secret

--- app/jobs/get_gender_job.rb
+++ app/jobs/get_gender_job.rb
@@ -4,37 +4,42 @@ class GetGenderJob < ApplicationJob
   queue_as :default
 
   def perform(user)
-    uri = URI.parse('https://cleaner.dadata.ru/api/v1/clean/name')
-    request = Net::HTTP::Post.new(uri.path,
-                                  {
-                                    'Content-Type' => 'application/json',
-                                    'Authorization' => Rails.application.credentials.dadata_api_token,
-                                    'X-Secret' => Rails.application.credentials.dadata_secret
-                                  })
+    res = dadata_request(user)
 
-    data = user.full_name.to_s.to_json
-    request.body = "[#{data}]"
-
-    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
-      http.request(request)
-    end
+    gender = nil
 
     if !res.read_body.include? 'status'
       gender_unformat = JSON.parse(res.read_body)[0]['gender']
-
-      gender = if gender_unformat == 'Ж'
-                 :female
-               else
-                 :male
-               end
-
-      user.last_gender_update = DateTime.now
-      user.gender = gender
-      user.save!
-
-      { gender: gender, last_gender_update: user.last_gender_update.strftime('%d %B %Y %T %Z') }
+      gender = gender_unformat == 'Ж' ? :female : :male
     else
-      'error'
+      gender = :male
     end
+
+    user.last_gender_update = DateTime.now
+    user.gender = gender
+    user.save!
+
+    { gender: gender, last_gender_update: user.last_gender_update.strftime('%d %B %Y %T %Z') }
+  end
+
+  private
+
+  def dadata_request(user)
+    uri = URI.parse('https://cleaner.dadata.ru/api/v1/clean/name')
+
+    creds = Rails.application.credentials
+
+    header = {
+      'Content-Type' => 'application/json',
+      'Authorization' => creds.dadata_api_token,
+      'X-Secret' => creds.dadata_secret
+    }
+
+    request = Net::HTTP::Post.new(uri.path, header)
+
+    data = user.full_name.to_json
+    request.body = "[#{data}]"
+
+    Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') { |http| http.request(request) }
   end
 end
